c7f25b0552477151e7d3be78f6cb2b2308f6545b
niranjan-bhat <68012374+niranjan-bhat@users.noreply.github.com>
2022-03-02 21:34:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 
+
 adviceCount = 0;
 adviceCountHtmlElement = document.querySelector('#advice-count');
 adviceHtmlElement = document.querySelector('.advice');
 btn = document.querySelector('.btn');
 btn.addEventListener('click',()=>{
+    fetchAdviceFromServer();
+});
+
+function fetchAdviceFromServer(){
     fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
         adviceCountHtmlElement.innerHTML = ++adviceCount;
-        adviceHtmlElement.innerHTML = data.slip.advice;
+        adviceHtmlElement.innerHTML = "\""+data.slip.advice+"\"";
 
     });
-});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchAdviceFromServer();
+  });
